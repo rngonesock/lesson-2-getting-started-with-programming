1a2d8bcbe814478d8965cca4098f0a552987c03c
rudra_gadila <rudragadila@gmail.com>
2025-02-02 19:35:07 -0500

    Variables Homework

---
+++ homework.py
@@ -0,0 +1,8 @@
+Rudra = "April 24"
+Shireesha = "Feb 27"
+Raj = "Dec 30"
+Reya = "April 10"
+print("My Birthday is on",Rudra)
+print("My Moms Birthday is on", Shireesha)
+print("My Dads Birthday is on", Raj)
+print("My Sisters Birthday is on", Reya)
